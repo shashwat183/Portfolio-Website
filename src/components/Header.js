bf882a528332ae76ad9a2a4a3851f65d60fb95f0
shashwat pragya <shashwat183@gmail.com>
2022-04-05 01:58:01 +0530

    Replace header links with react scroll Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,23 @@
 import './Header.css';
+import { Link } from 'react-scroll';
 
 function Header() {
   return (
   <div className="header" id="Home">
     <h1>Shashwat Pragya</h1>
     <div className='header-right'>
-      <a href='#Home'>Home</a>
-      <a href='#Projects'>Projects</a>
-      <a href='#CV'>CV</a>
-      <a href='#Links'>Social Links</a>
+      <Link to='Home' smooth={true} className="header-link">
+        Home
+      </Link>
+      <Link to='Projects' smooth={true} className="header-link">
+        Projects
+      </Link>
+      <Link to='CV' smooth={true} className="header-link">
+        CV
+      </Link>
+      <Link to='Links' smooth={true} className="header-link">
+        Social Links
+      </Link>
     </div>
   </div>
   );
